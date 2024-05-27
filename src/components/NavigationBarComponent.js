1ae2a768a120e1e2f072cb5fee60e1c6ccae5a3a
JDhyeok <jdhyeok97@gmail.com>
2024-05-27 22:21:33 +0900

    feat: add main-page

---
+++ src/components/NavigationBarComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavigationBarComponent = () => {
+  return (
+    <div style={{borderRight: '1px solid black'}}>
+      <h2>Dashboard</h2>
+			<h2>Create New Connector</h2>
+    </div>
+  );
+};
+
+export default NavigationBarComponent;
